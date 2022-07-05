1df43143e46d328c6041945f0331f7bc21a05dc7
Emmascobar <emmanuelrescobar@gmail.com>
2022-07-05 18:55:12 +0200

    IG

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+let Gastos = [950, 2100, 4800]
+
+let TotalGastos = 0;
+for (let i = 0; i < 3; i++) {
+    TotalGastos = TotalGastos + Gastos[i]
+}
+
+let promedioGastos = TotalGastos / 3;
+console.log(promedioGastos);
+
+let netoMensual = TotalGastos - 5000;
+console.log(netoMensual)
+
+if (promedioGastos > 5000) {
+    console.log("Has superado tu limite mensual de gastos")
+} else {
+    console.log("Aun te quedan" + " $ " + (netoMensual) + " " + "para gastar")
+}
+
+
+const ElColorFavoritoDeEmma = "Rojo";
+
+switch (ElColorFavoritoDeEmma) {
+    case "Azul":
+        console.log("Incorrecto, aunque es otro primario!");
+        break;
+    case "Verde ":
+        console.log("Incorrecto, casi!");
+        break;
+    case "Amarillo":
+        console.log("Incorrecto!");
+        break;
+    case "Rojo":
+        console.log("Felicitaciones acertaste!");
+        break;
+    default:
+        console.log("No me conoces nada");
+        break;
+}
+
